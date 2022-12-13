6241af2aac2456522577602acf13dee1c2a68bee
nadia <nadia@DESKTOP-5P9RLI9.fios-router.home>
2022-12-12 20:40:13 -0800

    first commit

---
+++ facebooktest/src/main/java/facebooktest/facebooktest/App.java
@@ -0,0 +1,31 @@
+package facebooktest.facebooktest;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+public class App 
+{
+	public static WebDriver driver;
+    public static void main( String[] args ) throws InterruptedException
+    {
+    	System.setProperty("webdriver.chrome.driver", "C:\\Users\\nadia\\eclipse-workspace\\seleniumproject1\\Driver\\chromedriver.exe");
+        driver=new ChromeDriver(); 
+    	
+    	driver.get("https://www.facebook.com/");
+    
+    	driver.manage().window().maximize();
+    	Thread.sleep(5000);
+    	
+  List<WebElement> Links=	driver.findElements(By.tagName("a"));
+       for(int i=0; i<Links.size()-1;i++) {
+System.out.println(Links.get(i).getAttribute("href"));
+
+    }
+}
+
+}
